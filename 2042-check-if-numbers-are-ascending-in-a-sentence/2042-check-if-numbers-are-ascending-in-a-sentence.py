eab81540f082e90b080b9545f8d940a30429c117
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-08-09 14:06:17 +0530

    Time: 45 ms (59.86%), Space: 13.9 MB (12.50%) - LeetHub

--- 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.py
@@ -5,9 +5,6 @@ class Solution:
         for i in x:
             if i.isdigit():
                 l.append(int(i))
-        print(l)
-        y,m = sorted(l) , l[0]
-        print(y)
         for j in range(1,len(l)):
             if l[j] == l[j-1]:
                 return False
