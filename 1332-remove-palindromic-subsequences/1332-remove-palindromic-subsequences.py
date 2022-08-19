f58c62698b7a2760a5606486931b148ba8f5beab
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-08-19 20:40:19 +0530

    Time: 59 ms (17.35%), Space: 14 MB (9.45%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -1,5 +1,4 @@
 class Solution:
     def removePalindromeSub(self, s: str) -> int:
-        if s == s[::-1]:
-            return 1
-        return 2
+        if s == s[::-1]: return 1
+        else: return 2
