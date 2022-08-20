4a983fbaec454be39e945aa0869a24ec6700df81
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-08-20 23:42:37 +0530

    Time: 35 ms (87.74%), Space: 13.9 MB (11.52%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,13 +1,6 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        # l = []
-        # for i in jewels:
-        #     if i in stones:
-        #         l.append(stones.count(i))
-        # if l == []:
-        #     return 0
-        # return sum(l)
         l = [stones.count(i) for i in jewels if i in stones]
         if l == []: return 0
-        else: return sum(l)
+        return sum(l)
         
