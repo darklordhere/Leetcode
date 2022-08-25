ea2d4367b78cad311f101e8d77b5a2aa37d54fcd
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-08-25 09:48:26 -0700

    Time: 101 ms (51.12%), Space: 14.2 MB (17.67%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -1,7 +1,6 @@
 class Solution:
     def findFinalValue(self, n: List[int], o: int) -> int:
         n = sorted(n)
-        print(n)
         for i in range(len(n)) :
             if o == n[i]:
                 o *= 2
