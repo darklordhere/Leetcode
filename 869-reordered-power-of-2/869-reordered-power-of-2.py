26058c1e1c6f8d15d05507aa214ad8b4708a8e05
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-08-26 09:41:52 -0700

    Time: 4171 ms (15.51%), Space: 85.6 MB (5.88%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -1,10 +1,17 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
+        def isp( n: int) -> bool:
+            if n <= 0:
+                return False
+            else:
+                while n > 1:
+                    n /= 2
+                return n == 1
         all = set(itertools.permutations(str(n)))
         for per in all:
             if per[0] == '0':
                 continue
-            x = int(''.join(per))
-            if (x and (not(x & (x - 1))) ):
-                return True
+            else:
+                if (isp(int(''.join(per)))) == True :
+                    return True
         return False
