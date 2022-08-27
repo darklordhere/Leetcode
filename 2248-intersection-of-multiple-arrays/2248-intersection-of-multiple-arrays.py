482c46f6d80606680c96dbd6632ddc60f6b5543c
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-08-27 20:54:14 +0530

    Time: 136 ms (22.58%), Space: 14.5 MB (6.95%) - LeetHub

---
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intersection(self, n: List[List[int]]) -> List[int]:
+        l = []
+        x = []
+        for i in n:
+            for j in i:
+                if j not in l:
+                    l.append(j)
+        for i in l:
+            for j in n:
+                if i not in j:
+                    x.append(i)
+                    break
+        return sorted(list(set(l) - set(x)))
