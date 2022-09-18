9d1c4b58bbffb3110a976493f3dc6eb5da607031
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-09-18 19:29:08 +0530

    Time: 158 ms (80.01%), Space: 16.2 MB (10.15%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -1,18 +1,14 @@
 class Solution:
     def trap(self, h: List[int]) -> int:
-        s = 0
-        x = [0]*len(h)
-        # taking lenth of list for loop
-        l, mlx , mrx = len(h) , [0]*len(h) , [0]*len(h)
-        # finding max of left and right from index 0 and index -1
+        lh= len(h) # initialising length of array
+        # initialising list for updation 
+        x , mlx , mrx = [0]*lh , [0]*lh , [0]*lh
+        # To find max from left and right we initialise mrx , mlx
         mrx[0] , mlx[-1] = h[0] , h[-1]
-        # looping through height to find max 
         for i in range(1,len(mrx)):
-            mrx[i] = max(h[i],mrx[i-1])
-        print(mrx)
+            mrx[i] = max(h[i],mrx[i-1]) # max from right
         for i in range(len(mlx)-2,-1,-1):
-            mlx[i] = max(mlx[i+1],h[i])
-        print(mlx)
+            mlx[i] = max(mlx[i+1],h[i]) # max from left
         for i in range(len(x)):
-            x[i] = min(mlx[i], mrx[i])-h[i]
-        return sum(x)
+            x[i] = min(mlx[i], mrx[i])-h[i] # for storing water unit/building
+        return sum(x) #sum of all units
