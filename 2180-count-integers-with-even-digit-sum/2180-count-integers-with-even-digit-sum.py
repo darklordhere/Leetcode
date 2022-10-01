9f9d5b9942f45ae2ad2f5736b72f8e75bf4f4a4b
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-01 14:34:14 +0530

    Time: 107 ms (15.47%), Space: 14 MB (11.12%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
@@ -3,7 +3,6 @@ class Solution:
         #x,c = [str(i) for i in range(2,n+1)] , 0
         c = 0
         for i in range(2,n+1):
-            i = str(i)
-            l = [int(j) for j in i]
+            i = str(i) ; l = [int(j) for j in i]
             if sum(l) % 2 == 0: c += 1
         return c
