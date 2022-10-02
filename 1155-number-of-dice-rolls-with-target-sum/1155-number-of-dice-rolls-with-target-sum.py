4d28fb31a725dc7b995d597d21d864a23f1f4e55
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-02 21:20:36 +0530

    Time: 1337 ms (24.55%), Space: 14.3 MB (84.81%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -19,27 +19,21 @@
 #         dp[n][t] == ans % mod
 #         return ans % mod
 class Solution:
-    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        dp = [[None for i in range(target+1)] for j in range(n+1)]
+    def numRollsToTarget(self, n: int, k: int, t: int) -> int:
+        dp = [[None for i in range(t+1)] for j in range(n+1)]
         MOD = 1000000007
-        return self.dfs(n,k,target,MOD,dp)
-        
-        
-    def dfs(self,n,k,target,MOD,dp):
+        return self.dfs(n,k,t,MOD,dp)
+    def dfs(self,n,k,t,MOD,dp):
         if n == 0:
-            if target == 0:
+            if t == 0:
                 return 1
-            
             return 0
-        
-        if target < 0:
+        if t < 0:
             return 0
-        if dp[n][target]!=None:
-            return dp[n][target]
+        if dp[n][t]!=None:
+            return dp[n][t]
         ans = 0
         for i in range(1,k+1):
-            ans = (ans + self.dfs(n-1,k,target-i,MOD,dp))%MOD
-            
-        
-        dp[n][target] = ans%MOD
+            ans = (ans + self.dfs(n-1,k,t-i,MOD,dp))%MOD
+        dp[n][t] = ans%MOD
         return ans%MOD
