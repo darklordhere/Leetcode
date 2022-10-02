587751b64be6c306944181bd5c9986d2561764d1
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-02 19:09:32 +0530

    Time: 29 ms (78.07%), Space: 13.9 MB (8.97%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def multiply(self, n1, n2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(n1)*int(n2))
+        
