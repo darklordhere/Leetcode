f17756598d499fc2d63ac6820c4a122aa376733f
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-02 21:24:22 +0530

    Time: 694 ms (60.71%), Space: 14.3 MB (86.69%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,28 +1,9 @@
-# class Solution:
-#     def numRollsToTarget(self, n: int, k: int, t: int) -> int:
-#         dp = [[None for i in range(t+1)] for j in range(n+1)]
-#         mod = 1000000007
-#         return self.dfs(n,k,t,mod,dp)
-
-#     def dfs(self,n,k,t,mod,dp):
-#         if n == 0:
-#             if t == 0:
-#                 return 1
-#             return 0
-#         if t < 0:
-#             return 0
-#         if dp[n][t] != None :
-#             return dp[n][t]
-#         ans = 0
-#         for i in range(1,k+1):
-#             ans = (ans+self.dfs(n-1,k,t-i,mod,dp))%mod
-#         dp[n][t] == ans % mod
-#         return ans % mod
 class Solution:
     def numRollsToTarget(self, n: int, k: int, t: int) -> int:
         dp = [[None for i in range(t+1)] for j in range(n+1)]
         mod = 1000000007
         return self.dfs(n,k,t,mod,dp)
+
     def dfs(self,n,k,t,mod,dp):
         if n == 0:
             if t == 0:
@@ -30,10 +11,10 @@ class Solution:
             return 0
         if t < 0:
             return 0
-        if dp[n][t]!=None:
+        if dp[n][t] != None :
             return dp[n][t]
         ans = 0
         for i in range(1,k+1):
-            ans = (ans + self.dfs(n-1,k,t-i,mod,dp))%mod
-        dp[n][t] = ans%mod
-        return ans%mod
+            ans = (ans+self.dfs(n-1,k,t-i,mod,dp))%mod
+        dp[n][t] = ans % mod
+        return ans % mod
