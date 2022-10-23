624c1c6dc89325fb3697f91e7915e9cfc14620cc
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-10-23 20:03:33 +0530

    Time: 580 ms (19.32%), Space: 16.5 MB (6.35%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,3 +1,3 @@
 class Solution:
     def findErrorNums(self, n: List[int]) -> List[int]:
-        return [ mode(n), list(set([i for i in range(1,len(n)+1) ]) - set(n))[0] ]
+        return [mode(n), list(set([i for i in range(1,len(n)+1) ]) - set(n))[0]]
