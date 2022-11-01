d09efed3cc4f3691325b6d48e3cd5624f287f2ef
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-01 23:10:18 +0530

    Time: 153 ms (36.44%), Space: 15.1 MB (53.71%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,7 +2,5 @@ class Solution:
     def twoSum(self, n: List[int], t: int) -> List[int]:
         x = {}
         for i,j in enumerate(n):
-            d = t-j
-            if d in x:
-                return [x[d],i]
+            if t-j in x: return [x[t-j],i]
             x[j] = i
