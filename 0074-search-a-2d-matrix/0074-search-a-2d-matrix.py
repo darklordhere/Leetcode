7d14dcdddc22bae53dee8a9f8e08ad99b2848cbe
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-07 19:40:39 +0530

    Time: 78 ms (70.26%), Space: 14.5 MB (43.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,6 +1,7 @@
 class Solution:
     def searchMatrix(self, m: List[List[int]], t: int) -> bool:
-        return any([t in i for i in m])
+        return t in sum(m,[])
+        # return any([t in i for i in m])
         # for i in m:
         #     if t in i:
         #         return True
