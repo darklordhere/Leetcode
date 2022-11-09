0e6e950cd5e12a1e93cb7a21dd3d8556a91ef737
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-09 20:06:46 +0530

    Time: 65 ms (40.36%), Space: 13.9 MB (64.66%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,11 +1,5 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        x = str(x)
-        if x[0] == "-":
-            r = -int(x[1:][::-1])
-        else:
-            r = int(x[::-1])
-        if r <= -(2**31) or r >= (2**31-1):
-            return 0
-        else:
-            return r
+        x = str(x) ; r = -int(x[1:][::-1]) if x[0] == "-" else int(x[::-1])
+        if r <= -(2**31) or r >= (2**31-1): return 0
+        else: return r
