6530b373c8b18535f189fea657aab7a5fedec1a7
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-17 21:46:13 +0530

    Time: 140 ms (33.28%), Space: 14.1 MB (30.03%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -3,8 +3,6 @@ class Solution:
         c = 0
         for i in range(len(n)-1):
             for j in range(i+1,len(n)):
-                if n[i]+n[j] == t:
-                    c += 1
-                if n[j]+n[i] == t:
-                    c += 1
+                if n[i]+n[j] == t: c += 1
+                if n[j]+n[i] == t: c += 1
         return c
