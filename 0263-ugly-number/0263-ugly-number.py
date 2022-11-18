efad8036f8f739b4cb3b91407d083837cbe9bcc1
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-18 19:17:32 +0530

    Time: 74 ms (11.52%), Space: 13.9 MB (60.08%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,17 +1,14 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        # my fast brute force
-        # c = 2
-        # if n in (2,3,5): return True
-        # if n <= 0: return False
-        # while n > 1:
-        #     if n % c == 0 and c in (2,3,4,5): n /= c
-        #     elif c not in (2,3,4,5): return False ; break
-        #     else: c += 1
-        # return True
-        
-        return n > 0 == 30**32 % n
-        
+        # fast brute force
+        c = 2
+        if n in (2,3,5): return True
+        if n <= 0: return False
+        while n > 1:
+            if n % c == 0 and c in (2,3,4,5): n /= c
+            elif c not in (2,3,4,5): return False ; break
+            else: c += 1
+        return True
 
     
             
