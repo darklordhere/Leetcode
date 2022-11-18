7a77b6d5a432102f7755b48d9ac21c26344747c6
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-18 19:08:01 +0530

    Time: 68 ms (25.67%), Space: 13.9 MB (60.08%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,12 +1,19 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
+        l=[2,3,4,5]
+        dp=defaultdict()
+        
+        for i in l:
+            dp[i]=1
+        
         c = 2
         if n in (2,3,5): return True
         if n <= 0: return False
         while n > 1:
-            if n % c == 0 and c in (2,3,4,5): n /= c
-            elif c not in (2,3,4,5): return False ; break
+            if n % c == 0 and c in dp: n /= c
+            elif c not in dp: return False ; break
             else: c += 1
         return True
+
     
             
