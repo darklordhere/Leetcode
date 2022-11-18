4a80ff7e5388218fea4817e863c01f65580ed69f
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-18 19:04:35 +0530

    Time: 35 ms (91.75%), Space: 13.9 MB (12.22%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -4,12 +4,9 @@ class Solution:
         if n in (2,3,5): return True
         if n <= 0: return False
         while n > 1:
-            if n % c == 0 and c in (2,3,4,5):
-                n /= c
-            elif c not in (2,3,4,5):
-                return False
-                break
-            else:
-                c += 1
+            if n % c == 0 and c in (2,3,4,5): n /= c
+            elif c not in (2,3,4,5): return False ; break
+            else: c += 1
         return True
+    
             
