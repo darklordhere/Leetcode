76f4a7820d3bd2645f265cdc26fbba5c4245cb05
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-11-30 20:07:55 +0530

    Time: 80 ms (19.19%), Space: 14 MB (72.74%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,9 +1,7 @@
 class Solution:
     def uniqueOccurrences(self, a: List[int]) -> bool:
-        x = Counter(a)
-        l = set()
+        x = Counter(a) ; l = set()
         for i,j in x.items():
-            if j in l:
-                return False
+            if j in l: return False
             l.add(j)
         return True
