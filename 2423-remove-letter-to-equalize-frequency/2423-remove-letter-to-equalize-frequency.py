1fafa2bedbeb28695dafcd4e6243141652f21181
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2022-12-20 16:05:53 +0530

    Time: 57 ms (44.24%), Space: 13.9 MB (17.02%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -1,6 +1,9 @@
 class Solution:
-    def equalFrequency(self, w: str) -> bool:
-        for i in range(len(w)):
-            if len(set(Counter(w[:i]+w[i+1:]).values())) == 1:
-                return True
+    def equalFrequency(self, word: str) -> bool:
+        dct = collections.Counter(word)
+        for el in word:
+            dct[el]-=1
+            if not dct[el]: del dct[el]
+            if len(set(dct.values())) == 1: return True
+            dct[el]+=1
         return False
