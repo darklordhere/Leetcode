9db1a4a375535d77701cd8ce1c8edda6cacb1106
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-04 13:39:12 +0530

    Time: 3295 ms (14.62%), Space: 15.2 MB (91.98%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.py
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.py
@@ -1,7 +1,13 @@
 class Solution:
     def longestPrefix(self, s: str) -> str:
-        i = 1
-        while True:
-            if s[i:] == s[:-i]:
-                return s[i:]
-            i += 1
+        # i = 1
+        # while True:
+        #     if s[i:] == s[:-i]:
+        #         return s[i:]
+        #     i += 1
+        
+        res = ''
+        for i in range(1, len(s)):
+            if s[:i] == s[-i:]:
+                res = s[:i]
+        return res
