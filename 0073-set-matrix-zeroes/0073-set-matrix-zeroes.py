827163de79d77c4979bdf61b558a53e978ec20fe
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-06 19:43:16 +0530

    Time: 181 ms (57.64%), Space: 14.6 MB (93.66%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,21 +3,15 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # l = []
-        # for i in range(len(m)):
-        #     # print(i,j)
-        #     for p,q in enumerate(m[i]):
-        #         # print(p,q)
-        #         if q == 0:
-        #             l.append((i,p))
-        # print(l)
         lr , lc , r , c = len(m) , len(m[0]) , set() , set()
         for i in range(lr):
             for j in range(lc):
                 if m[i][j] == 0:
-                    r.add(i) ; c.add(j)
+                    r.add(i)
+                    c.add(j)
         for i in range(lr):
             for j in range(lc):
                 if i in r or j in c:
                     m[i][j] = 0
+        
         
