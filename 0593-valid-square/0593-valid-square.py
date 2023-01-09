509d9e90f4f78138b8837f429a63fd35cd728b11
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-09 18:18:32 +0530

    Time: 37 ms (84.73%), Space: 13.9 MB (94.41%) - LeetHub

---
+++ 0593-valid-square/0593-valid-square.py
@@ -0,0 +1,15 @@
+class Solution:
+    def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
+        def fun(p1,p2):
+            a,b = p1 ; c,d = p2
+            return ((((a-c)**2)+((b-d)**2))**0.5)
+        s = set()
+        s.add(fun(p1,p2));s.add(fun(p1,p3))
+        s.add(fun(p1,p4));s.add(fun(p2,p3))
+        s.add(fun(p2,p4));s.add(fun(p3,p4))
+        if 0 in s: return False
+        else: return len(s) == 2
+
+        
+
+        
