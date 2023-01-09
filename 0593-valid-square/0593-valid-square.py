f5b1dd2e36a93107565a2c0a20dec6830666c4a3
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-09 18:19:18 +0530

    Time: 46 ms (63.31%), Space: 14 MB (51.02%) - LeetHub

--- 0593-valid-square/0593-valid-square.py
+++ 0593-valid-square/0593-valid-square.py
@@ -4,9 +4,8 @@ class Solution:
             a,b = p1 ; c,d = p2
             return ((((a-c)**2)+((b-d)**2))**0.5)
         s = set()
-        s.add(fun(p1,p2));s.add(fun(p1,p3))
-        s.add(fun(p1,p4));s.add(fun(p2,p3))
-        s.add(fun(p2,p4));s.add(fun(p3,p4))
+        s.add(fun(p1,p2));s.add(fun(p1,p3));s.add(fun(p1,p4))
+        s.add(fun(p2,p3));s.add(fun(p2,p4));s.add(fun(p3,p4))
         if 0 in s: return False
         else: return len(s) == 2
 
