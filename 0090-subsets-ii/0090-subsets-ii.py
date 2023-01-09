9b14a6203efb0cdae148e48eb63a0c89fe87ccc7
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-09 19:08:58 +0530

    Time: 50 ms (59.58%), Space: 14.1 MB (93.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -2,8 +2,6 @@ class Solution:
     def subsetsWithDup(self, n: List[int]) -> List[List[int]]:
         l = [[]]
         for i in range(1,len(n)+1):
-            x = combinations(n,i)
-            for j in x:
-                if sorted(j) not in l:
-                    l.append(sorted(j))
+            for j in combinations(n,i):
+                if sorted(j) not in l: l.append(sorted(j))
         return l
