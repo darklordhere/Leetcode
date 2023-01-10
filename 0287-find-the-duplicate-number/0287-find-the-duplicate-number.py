79780d5988f65d1c8deb5296b02462f965b389ef
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-10 20:22:39 +0530

    Time: 656 ms (81.34%), Space: 35.8 MB (7.41%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,3 +1,16 @@
 class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        return mode(nums)
+    def findDuplicate(self, n: List[int]) -> int:
+        return mode(n)
+        
+        
+#         for i in range(1,len(n)):
+#             if n[i] in n[:i] or n[i] in n[i+1:]:
+#                 return n[i]
+
+
+        # i = 1
+        # while i != len(n):
+        #     if n[i] in n[:i] or n[i] in n[i+1:]: return n[i]
+        #     else: i += 1
+        
+            
