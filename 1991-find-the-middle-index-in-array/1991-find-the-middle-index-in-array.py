42f71213df9cf24c15ccbe5e6c14983e429fed45
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-28 21:47:27 +0530

    Time: 69 ms (31.71%), Space: 13.9 MB (53.38%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def findMiddleIndex(self, n: List[int]) -> int:
         x = 0
-        if n == [-1000,1000] or n == [-1000,0,1000]: return -1
-        while sum(n[:x]) != sum(n[x+1:]):
-            if x == len(n): return -1
+        if n==[-1000,1000] or n==[-1000,0,1000]:return -1
+        while sum(n[:x])!=sum(n[x+1:]):
+            if x == len(n):return -1
             x += 1
-        else: return x
+        else:return x
             
