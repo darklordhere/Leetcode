8a68fc998123a84ce994095bffc299f2e59e1f6b
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-28 20:48:47 +0530

    Time: 42 ms (35.29%), Space: 14 MB (14.89%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -1,9 +1,5 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        if k == 1:
-            return 0
+        if k == 1: return 0
         m = 2**(n-1)//2
-        if k <= m:
-            return int(self.kthGrammar(n-1,k))
-        else:
-            return int(1-self.kthGrammar(n-1,k-m))
+        return (int(self.kthGrammar(n-1,k)) if k<=m else int(1-self.kthGrammar(n-1,k-m)))
