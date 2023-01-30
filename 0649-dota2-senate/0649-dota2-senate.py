5eb962aef987482656b55b255fdfe34fcaf5d22b
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-30 20:42:43 +0530

    Time: 115 ms (51.78%), Space: 14.4 MB (10.15%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def predictPartyVictory(self, s: str) -> str:
+        l = len(s)
+        r = [i for i in range(l) if s[i] == 'R']
+        d = [i for i in range(l) if s[i] == 'D']
+        while len(d) and len(r):
+            if d[0] > r[0]: r.append(r[0] + l)
+            else: d.append(d[0] + l)
+            del d[0]
+            del r[0]
+        return "Dire" if len(d) != 0 else "Radiant"
