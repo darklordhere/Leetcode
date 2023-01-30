cbf9be7258a64836098e6a51d2fe095e050b3048
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-30 20:52:20 +0530

    Time: 109 ms (53.81%), Space: 14.4 MB (10.15%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -4,8 +4,10 @@ class Solution:
         r = [i for i in range(l) if s[i] == 'R']
         d = [i for i in range(l) if s[i] == 'D']
         while len(d) and len(r):
-            if d[0] > r[0]: r.append(r[0] + l)
-            else: d.append(d[0] + l)
+            if d[0] > r[0]:
+                r.append(r[0] + l)
+            else: 
+                d.append(d[0] + l)
             del d[0]
             del r[0]
         return "Dire" if len(d) != 0 else "Radiant"
