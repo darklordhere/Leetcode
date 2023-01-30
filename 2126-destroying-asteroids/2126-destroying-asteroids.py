77e1aa0cde33bc8393317b15e13a5963723f67ec
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-01-30 20:14:53 +0530

    Time: 1176 ms (57.88%), Space: 27.7 MB (89.41%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,9 +1,7 @@
 class Solution:
     def asteroidsDestroyed(self, m: int, a: List[int]) -> bool:
         for i in sorted(a):
-            if m >= i:
-                m += i
-            else:
-                return False
+            if m >= i: m += i 
+            else: return False
         return True
         
