2b703a5edb0bd4dcd7fb0c5e3886e3bad04626cf
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-02 20:36:32 +0530

    Time: 40 ms (63.42%), Space: 13.9 MB (77.66%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAlienSorted(self, w: List[str], o: str) -> bool:
+        t = "".maketrans(o,string.ascii_lowercase)
+        for i in range(len(w)):
+            w[i] = w[i].translate(t)
+        return w == sorted(w)
+        
