8e6c71adfbcf920bb960d54b2ff27659da2c1b32
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-02 20:48:10 +0530

    Time: 29 ms (94.15%), Space: 13.8 MB (55.87%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -2,5 +2,4 @@ class Solution:
     def removeElement(self, n: List[int], v: int) -> int:
         while v in n:
             n.remove(v)
-        else:
-            return len(n)
+        return len(n)
