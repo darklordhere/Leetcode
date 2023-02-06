fca9708b68ce01618316c9d0b79257c4a29bf9a7
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-06 19:15:11 +0530

    Time: 86 ms (25.80%), Space: 14.1 MB (33.52%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,9 +1,6 @@
 class Solution:
     def shuffle(self, n: List[int], k: int) -> List[int]:
-        a,b = n[:k] , n[k:] ; l = [] ; i = 0
-        while k != i:
-            l.append(a[i])
-            l.append(b[i])
-            i += 1
+        l = [] ; i = 0
+        while k != i: l.append(n[:k][i]) ; l.append(n[k:][i]) ; i += 1
         return l
         
