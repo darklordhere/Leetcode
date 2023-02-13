f8d734646c1fc23424e37c79cc0870d832caebc2
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-13 21:33:40 +0530

    Time: 91 ms (44.40%), Space: 19.6 MB (5.98%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,7 +1,3 @@
 class Solution:
     def permuteUnique(self, n: List[int]) -> List[List[int]]:
-        l = []
-        for i in permutations(n):
-            if i not in l:
-                l.append(i)
-        return l
+        return set([i for i in permutations(n)])
