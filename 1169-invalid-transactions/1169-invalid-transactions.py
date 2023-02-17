4dc6c09ed1de6e5f61c033b1484393fa1ac918ec
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-17 17:03:43 +0530

    Time: 133 ms (46.07%), Space: 14.4 MB (92.24%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -9,5 +9,4 @@ class Solution:
                     name2, time2, amount2, city2 = t2.split(',')
                     if name1 == name2 and city1 != city2 and abs(int(time1) - int(time2)) <= 60:
                         invalid.append(t1);break
-        
         return invalid
