f2453ae725364f47c06fac6b5289ed6cfbce45dc
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-20 17:50:23 +0530

    Time: 60 ms (22.26%), Space: 14.7 MB (73.66%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,14 +1,10 @@
 class Solution:
     def searchInsert(self, n: List[int], t: int) -> int:
-        if t in n:
-            return n.index(t)
+        if t > n[-1]: return len(n)
+        elif t < n[0]: return 0
         else:
-            if t > n[-1]:
-                return len(n)
-            elif t < n[0]:
-                return 0
-            else:
-                for i in n:
-                    if i > t:
-                        return n.index(i)
+            for i in n:
+                if i >= t: return n.index(i)
+
+        # return bisect.bisect_left(nums,target)
         
