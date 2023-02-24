753e7cb4ab21a5fb951948039ea596910b5e2fde
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-02-24 14:52:14 +0530

    Time: 30 ms (75.74%), Space: 13.8 MB (40.62%) - LeetHub

--- 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
+++ 2520-count-the-digits-that-divide-a-number/2520-count-the-digits-that-divide-a-number.py
@@ -2,5 +2,6 @@ class Solution:
     def countDigits(self, n: int) -> int:
         c = 0
         for i in str(n):
-            if n%int(i)==0: c += 1
+            if n%int(i)==0: 
+                c += 1
         return c
