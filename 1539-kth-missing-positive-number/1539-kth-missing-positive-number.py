71ac3215818000b4573988107ed2f23e808b5c4d
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-03-06 20:25:04 +0530

    Time: 172 ms (16.72%), Space: 14 MB (80.43%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def findKthPositive(self, l: List[int], k: int) -> int:
-        li = [] ; c = 0
+        c = 0
         for i in range(1,5000):
             if c < k:
                 if i not in l: c += 1
