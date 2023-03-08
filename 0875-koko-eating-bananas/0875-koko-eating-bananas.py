7c56e1806579f2bd90bc9c9a145f1a33b0d99b21
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-03-08 21:49:24 +0530

    Time: 360 ms (98.69%), Space: 15.4 MB (97.76%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minEatingSpeed(self, p: List[int], h: int) -> int:
+        l = 1
+        r = max(p)
+        while l <= r:
+            m = (l + r)//2
+            t = sum([-(-i//m) for i in p])
+            if t > h:
+                l = m + 1
+            else:
+                r = m - 1
+        return l
