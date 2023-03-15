27ddb1423af510207d2e2e49352f9cfbcba7bdd6
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-03-15 19:54:45 +0530

    Time: 123 ms (93.51%), Space: 14.8 MB (65.21%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -10,7 +10,6 @@ class Solution:
         for j in range(len(m)):
             for i in range(len(m[j])):
                 if m[j][i] == 0: c.append(i)
-                    
         for i in r: m[i] = [0]*len(m[i])
             
         for j in c:
