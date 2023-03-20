30627860507f21febe17ce873ec677c1a28ba402
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-03-20 21:41:25 +0530

    Time: 1302 ms (100.00%), Space: 33.5 MB (100.00%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -1,9 +1,6 @@
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        seen = [0] * (len(nums) + 1)
-        res = 0
+        seen = [0] * (len(nums) + 1) ; res = 0
         for a,i in sorted([a,i] for i,a in enumerate(nums)):
-            if not seen[i]: 
-                res += a
-                seen[i] = seen[i - 1] = seen[i + 1] = 1
+            if not seen[i]: res += a ; seen[i] = seen[i - 1] = seen[i + 1] = 1
         return res
