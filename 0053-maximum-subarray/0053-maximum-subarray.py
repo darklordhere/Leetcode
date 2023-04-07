14482731adb14fa1979ec056c2150ca570bf183c
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-04-07 21:37:40 +0530

    Time: 666 ms (95.15%), Space: 28.6 MB (83.03%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, n: List[int]) -> int:
+        c = 0 ; m = -99999
+        for i in n:
+            c += i
+            if c > m: 
+                m = c
+            if c < 0: c = 0
+        return m
