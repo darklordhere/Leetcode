c84acf8653675855b1adcb2fdd4bb821c82fea2a
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-04-19 21:23:11 +0530

    Time: 1030 ms (34.38%), Space: 34.1 MB (41.21%) - LeetHub

--- 2611-mice-and-cheese/2611-mice-and-cheese.py
+++ 2611-mice-and-cheese/2611-mice-and-cheese.py
@@ -3,7 +3,9 @@ class Solution:
     def compare(a, b): return a[0] > b[0]
     def miceAndCheese(self, r1: List[int], r2: List[int], k: int) -> int:
         l = [] ; ne = len(r1)
+        
         for w in range(ne):
+            
             l.append((r1[w] - r2[w], w))
         l.sort(key=lambda j: j[0], reverse=True) ; 
         
