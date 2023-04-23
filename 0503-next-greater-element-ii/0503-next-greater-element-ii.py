57364b2a691660a97e5821bb9f657bb9433c518d
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-04-23 22:47:37 +0530

    Time: 7043 ms (5.04%), Space: 15.9 MB (21.23%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def nextGreaterElements(self, n: List[int]) -> List[int]:
+        l = []
+        ar = []
+        for i in range(1,len(n)):
+            if n[i] > n[0]:
+                l.append(n[i])
+                break
+        else:
+            l.append(-1)
+        for i in range(1,len(n)):
+            x = n[i]
+            if i == len(n)-1:
+                for i in range(i):
+                    if n[i] > x:
+                        l.append(n[i])
+                        break
+                else:
+                    l.append(-1)
+            else:
+                ar[:] = n[i+1:]+n[:i]
+                for i in ar:
+                    if i > x:
+                        l.append(i)
+                        break
+                else:
+                    l.append(-1)
+        return l
+            
