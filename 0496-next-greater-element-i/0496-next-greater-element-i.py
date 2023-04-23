096bc5123d5132204af3b646c44e85fd35e01f1b
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-04-23 22:27:14 +0530

    Time: 75 ms (33.87%), Space: 14.1 MB (91.23%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextGreaterElement(self, n1: List[int], n2: List[int]) -> List[int]:
+        l = []
+        for i in n1:
+            x = n2.index(i)
+            if x == len(n2)-1:
+                l.append(-1)
+            else:
+                for j in range(x+1,len(n2)):
+                    if n2[j] > n2[x]:
+                        l.append(n2[j])
+                        break
+                else:
+                    l.append(-1)
+        return l
