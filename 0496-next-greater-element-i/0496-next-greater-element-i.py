41839969f6f0e44134952b7f027d0350dcda9caf
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-04-24 21:38:01 +0530

    Time: 86 ms (21.03%), Space: 14.1 MB (91.21%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -6,6 +6,7 @@ class Solution:
             if x == len(n2)-1: l.append(-1)
             else:
                 for j in range(x+1,len(n2)):
-                    if n2[j] > n2[x]: l.append(n2[j]);break
+                    if n2[j] > n2[x]:
+                        l.append(n2[j]);break
                 else: l.append(-1)
         return l
