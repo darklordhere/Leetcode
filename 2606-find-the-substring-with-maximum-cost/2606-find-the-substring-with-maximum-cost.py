ab7a2ee6d92e593f0e26e7db76863e1440f89d67
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-05-08 21:01:01 +0530

    Time: 220 ms (81.24%), Space: 18.1 MB (13.47%) - LeetHub

---
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mxm(self, l: List[int], sz: int) -> int:
+        mf = float('-inf') ; jj = 0
+        for i in range(sz):
+            jj += l[i]
+            if mf < jj: mf = jj
+                
+            if jj < 0: jj = 0
+                
+        return mf
+    def maximumCostSubstring(self, s: str, cs: str, vs: List[int]) -> int:
+        km = {} ;c = 'a'; km[c] = 1
+        for i in range(1, 26): c = chr(ord(c) + 1) ; km[c] = i + 1
+        for i in range(len(cs)): km[cs[i]] = vs[i]
+        va = []
+        for i in range(len(s)): va.append(km[s[i]])
+        r = self.mxm(va, len(va))
+        if r < 0: return 0
+        else: return r
+
+
