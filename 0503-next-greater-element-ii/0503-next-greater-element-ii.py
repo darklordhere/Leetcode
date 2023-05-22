dd17f190cdb54166088f9e78917c3be3f9bf363f
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-05-22 20:45:55 +0530

    Time: 6548 ms (5.03%), Space: 18.3 MB (10.55%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -3,20 +3,23 @@ class Solution:
         l = []
         ar = []
         for i in range(1,len(n)):
-            if n[i] > n[0]: l.append(n[i]);break
-        else: l.append(-1)
+            if n[i] > n[0]:
+                l.append(n[i]);break
+        else:
+            l.append(-1)
         for i in range(1,len(n)):
             x = n[i]
             if i == len(n)-1:
                 for i in range(i):
-                    if n[i] > x: l.append(n[i]);break
-                else: l.append(-1)
+                    if n[i] > x:
+                        l.append(n[i]);break
+                else:
+                    l.append(-1)
             else:
                 ar[:] = n[i+1:]+n[:i]
                 for i in ar:
                     if i > x:
-                        l.append(i)
-                        break
+                        l.append(i);break
                 else:
                     l.append(-1)
         return l
