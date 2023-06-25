ac7c37d84f00820cf6076a145c6f88352bc76cf3
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-06-25 13:40:04 +0530

    Time: 45 ms (82.84%), Space: 16.3 MB (68.43%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -5,6 +5,7 @@ class Solution:
         else:
             s.sort() ; r = ""
             for i in range(len(min(s[0],s[-1]))):
-                if s[0][i] != s[-1][i]: break
+                if s[0][i] != s[-1][i]: 
+                    break
                 else: r += s[0][i]
             return r
