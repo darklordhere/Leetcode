f0b1309284ad81068dda6aad715ad9eab8520d01
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-07-01 23:26:12 +0530

    Time: 53 ms (93.78%), Space: 16.4 MB (46.20%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -3,7 +3,6 @@ class Solution:
         x = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         t = 0
         for i in range(len(s)):
-            if i+1 < len(s) and x[s[i]] < x[s[i+1]]: 
-                t -= x[s[i]]
+            if i+1 < len(s) and x[s[i]] < x[s[i+1]]: t -= x[s[i]]
             else: t += x[s[i]]
         return t
