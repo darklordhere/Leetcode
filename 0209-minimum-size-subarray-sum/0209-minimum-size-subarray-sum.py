3397ca9088fa6a4d876fc064ed384878f6885909
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-07-06 20:51:55 +0530

    Time: 254 ms (86.32%), Space: 29.1 MB (41.05%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSubArrayLen(self, t: int, n: List[int]) -> int:
+        if sum(n) < t:
+            return 0
+        s,l,ans = 0,0,len(n)
+        for r,val in enumerate(n):
+            s += val
+            while s >= t:
+                s -= n[l]
+                ans = min(ans,r-l+1)
+                l += 1
+        return ans
