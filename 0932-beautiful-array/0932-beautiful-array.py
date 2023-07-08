7a1a4972759f250890cb7c134a62935a08c7f0dd
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-07-08 20:28:28 +0530

    Time: 56 ms (37.50%), Space: 16.5 MB (75.69%) - LeetHub

--- 0932-beautiful-array/0932-beautiful-array.py
+++ 0932-beautiful-array/0932-beautiful-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def beautifulArray(self, n: int) -> List[int]:
-        x = [1]
-        while len(x) < n: 
-            x = [i*2-1 for i in x]+[i*2 for i in x]
-        return [i for i in x if i <= n]
+        xy = [1]
+        while len(xy) < n: 
+            xy = [i*2-1 for i in xy]+[i*2 for i in xy]
+        return [i for i in xy if i <= n]
