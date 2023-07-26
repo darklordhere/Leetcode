4a5c91261efad9360255f67072090bae8950f270
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-07-26 19:34:19 +0530

    Time: 88 ms (42.88%), Space: 17.3 MB (24.36%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -3,18 +3,15 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        if 0 not in arr:
-            return arr
-        else:
-            x = [] ; i = 0
-            while i < len(arr):
-                if arr[i] == 0:
-                    x.append(arr[i])
-                    x.append(0)
-                else:
-                    x.append(arr[i])
-                i += 1
-            arr[:] = x[:len(arr)]
-            return arr
+        x = [] ; i = 0
+        while i < len(arr):
+            if arr[i] == 0:
+                x.append(arr[i])
+                x.append(0)
+            else:
+                x.append(arr[i])
+            i += 1
+        arr[:] = x[:len(arr)]
+        return arr
             
         
