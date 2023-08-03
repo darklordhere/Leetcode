dfd3d0f3b11e2e697113d625cca128641efa8b2e
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-08-03 20:13:01 +0530

    Time: 42 ms (78.23%), Space: 16.4 MB (57.40%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def letterCombinations(self, d: str) -> List[str]:
         dic = {
-            "1":"","2":"abc","3":"def",
-            "4":"ghi","5":"jkl","6":"mno",
-            "7":"pqrs","8":"tuv","9":"wxyz"
+            "1":"","2":"abc","3":"def", "4":"ghi", "5":"jkl",
+            "6":"mno", "7":"pqrs","8":"tuv","9":"wxyz"
         }
         return list(map("".join,product(*map(dic.get,d)))) if d else []
         
