3d85da8c2db049c70d0e0f47a9dde0645f0f7497
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-08-05 20:05:21 +0530

    Time: 87 ms (99.09%), Space: 17.6 MB (15.31%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,8 +3,7 @@ class Solution:
         s = []
         for a in ast:
             while s and a < 0 < (b := s[-1]):
-                if b <= -a:s.pop()
-                if b >= -a: 
-                    break
+                if b <= -a: s.pop()
+                if b >= -a:break
             else: s.append(a)
         return s
