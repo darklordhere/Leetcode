4759561854be8a6c9265d66a5ad7509733422535
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-08-08 20:00:41 +0530

    Time: 48 ms (85.83%), Space: 16.8 MB (38.67%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,8 +1,5 @@
 class Solution:
     def search(self, n: List[int], t: int) -> int:
-        if t not in n: return -1
-        else:
-            x = 0
-            while x != len(n):
-                if n[x] == t: return x
-                x+=1
+        if t in n:
+            return n.index(t)
+        return -1
