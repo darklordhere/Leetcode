8d2aa198640bdf0a15b16b8096efbc3db5add38a
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-08-20 19:18:33 +0530

    Time: 130 ms (100.00%), Space: 16.4 MB (100.00%) - LeetHub

---
+++ 2829-determine-the-minimum-sum-of-a-k-avoiding-array/2829-determine-the-minimum-sum-of-a-k-avoiding-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumSum(self, n: int, k: int) -> int:
+        x = [0]*n
+        a = 1 ; b = 0 ; z = 0
+        while b < n:
+            for i in x:
+                if i+a == k:
+                    if a == k:
+                        pass
+                    else:
+                        a += 1
+                        break
+            else:
+                print(a,b)
+                x[z] = a
+                a += 1
+                z += 1
+                b += 1
+        print(x)
+        return sum(x)
+            
