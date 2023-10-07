e3cc906e70c694897289e2269c1c984143e692e8
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-10-08 01:30:24 +0530

    Time: 43 ms (82.31%), Space: 16 MB (99.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -9,9 +9,9 @@ class Solution:
             if n[i] < n[i+1]:
                 ind = i
                 break
-        if ind == -1:
-            n[:] = n[::-1]
         else:
+            n[:] = n[::-1]
+        if ind > -1:
             n[:] = n[:ind+1] + sorted(n[ind+1:])
             for i in range(ind+1,len(n)):
                 if n[i] > n[ind]:
