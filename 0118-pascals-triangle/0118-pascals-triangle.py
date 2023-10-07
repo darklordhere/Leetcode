c8bd6bafd1dd851b117a3dc72553d003f9106c04
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-10-08 00:06:45 +0530

    Time: 39 ms (66.78%), Space: 16.1 MB (90.60%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,10 +1,27 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        ans = [(row:= [1])]
-
-        for _ in range(1, numRows):
-
-            row = list(map(sum,pairwise([0]+row+[0])))
-            ans.append(row)
-
-        return ans
+        # if numRows == 1:
+        #     return [[1]]
+        # elif numRows > 1 and numRows < 6:
+        #     r = [[1]]
+        #     for i in range(1,numRows):
+        #         r.append(map(int,str(11**i)))
+        #     return r
+        # else:
+        r = []
+        for i in range(1,numRows+1):
+            b = 1
+            t = []
+            for j in range(1,i+1):
+                t.append(b)
+                b = b*(i-j)//j
+            r.append(t)
+        return r
+            
+        # r = []
+        # for i in range(numRows):
+        #     t = ""
+        #     for j in range(numRows+1):
+        #         t += str((factorial(i))//(factorial(abs(i-j))*factorial(j)))
+        #     r.append([*map(int,str(t[:i-j]))])
+        # return r
