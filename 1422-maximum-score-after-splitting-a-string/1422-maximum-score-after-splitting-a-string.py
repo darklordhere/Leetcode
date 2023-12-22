b44cf4cb0ca2b703bc6857dd18f3cb16d1a26837
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2023-12-22 20:53:03 +0530

    Time: 100 ms (12.69%), Space: 17.4 MB (5.69%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -3,16 +3,19 @@ class Solution:
         m = 0
         def c1(s):
             x = 0
+            y = 0
             for i in s:
                 if i == "0":
                     x += 1
-            return x
-        def c2(s):
-            x = 0
-            for i in s:
-                if i == "1":
-                    x += 1
-            return x
+                else:
+                    y += 1
+            return [x,y]
+        # def c2(s):
+        #     x = 0
+        #     for i in s:
+        #         if i == "1":
+        #             x += 1
+        #     return x
         for i in range(1,len(s)):
-            m = max(c1(s[:i])+c2(s[i:]),m)
+            m = max(c1(s[:i])[0]+c1(s[i:])[1],m)
         return m
