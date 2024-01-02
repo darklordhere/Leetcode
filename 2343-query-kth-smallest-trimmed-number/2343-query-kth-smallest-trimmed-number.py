6db6b3c0b62a627d56f3b4777259e282be30fc0f
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-01-02 22:41:01 +0530

    Time: 1199 ms (7.82%), Space: 17.7 MB (28.49%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.py
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.py
@@ -1,26 +1,11 @@
 class Solution:
     def smallestTrimmedNumbers(self, n: List[str], q: List[List[int]]) -> List[int]:
 
-        l = []
         x = []
-        def fn(l,m,y):
-            i = y
-            r = 0
-            while y > 0 and i < len(l):
-                if l[i] == m:
-                    r = i
-                    y -= 1
-                i += 1
-            return r
-
         for i in q:
             l = [int(j[len(j)-i[1]:]) for j in n]  
             m = sorted(l)[i[0]-1]
-            # if l.count(m) > 1:
-                # if i[0]%2 == 0:
-                #     x.append(fn(l,m,i[0]))
-                # if i[0]%2 == 1:
-            if l.count(m) <= 1:
+            if l.count(m) < 2:
                 x.append(l.index(m))
             else:
                 p = [ i for i in range(len(l)) if l[i] == m]
