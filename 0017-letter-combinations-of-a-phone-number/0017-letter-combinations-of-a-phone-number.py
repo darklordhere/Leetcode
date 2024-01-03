e2f1976c93af8e09d449d424fe0bf4311396e864
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-01-03 21:39:04 +0530

    Time: 31 ms (91.57%), Space: 17.5 MB (10.77%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,9 +1,47 @@
 class Solution:
     def letterCombinations(self, d: str) -> List[str]:
+        
         dic = {
             "1":"","2":"abc","3":"def", "4":"ghi", "5":"jkl",
             "6":"mno", "7":"pqrs","8":"tuv","9":"wxyz"
         }
         
-        return list(map("".join,product(*map(dic.get,d)))) if d else []
+        r = []
+        def bk(i,curr):
+            if len(curr) == len(d):
+                r.append(curr)
+                return
+            for c in dic[d[i]]:
+                bk(i+1,curr+c)
+        if d:
+            bk(0,"")
+        return r
+            
+#         x = combinations("".join([dic[i] for i in d]),len(d))
+#         k = [dic[i] for i in d]
+#         m = "".join(k)
+#         # print(m)
+#         r = []
+#         for i in x:
+#             if "".join(i) not in m:
+#                 r.append("".join(i))
+#         r = r[1:len(r)-1]
+#         for i in range(len(k)):
+#             t = ""
+#             for j in range(i+1,len(k)):
+#                 t += k[i][-1]
+#                 t += k[j][0]
+#                 if len(t) == 2:
+#                     r.append(t)
+#                     break
+#         if d == "":
+#             return []
+#         elif len(d) == 1:
+#             return list(dic[d])
+#         else:
+#             return r
+        
+        
+        
+
         
