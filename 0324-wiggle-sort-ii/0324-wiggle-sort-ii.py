2003c3f41185b4f477da1385306861b15f841bd2
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-01-09 20:17:17 +0530

    Time: 133 ms (80.00%), Space: 20.6 MB (7.47%) - LeetHub

---
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def wiggleSort(self, n: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n.sort()
+        n[::2],n[1::2] = n[:len(n[::2])][::-1],n[len(n[::2]):][::-1]
