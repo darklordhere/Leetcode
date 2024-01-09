7f4ca75bf39b7fe2e540b3ddb694a8ecc63f4154
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-01-09 19:41:41 +0530

    Time: 416 ms (26.15%), Space: 17.4 MB (9.56%) - LeetHub

---
+++ 2970-count-the-number-of-incremovable-subarrays-i/2970-count-the-number-of-incremovable-subarrays-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def incremovableSubarrayCount(self, n: List[int]) -> int:
+        def fn(n):
+            return all(n[k]<n[k+1] for k in range(len(n)-1))
+        c = 0
+        for i in range(len(n)):
+            for j in range(i+1,len(n)+1):
+                if fn(n[:i]+n[j:]) == True:
+                    c += 1
+        return c
