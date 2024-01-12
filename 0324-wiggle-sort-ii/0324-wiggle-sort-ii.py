ad8fab51e71ea1474daca9277d40c52bbf77ccff
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-01-13 00:33:27 +0530

    Time: 120 ms (95.20%), Space: 20.6 MB (7.29%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.py
@@ -4,4 +4,5 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n.sort()
+        
         n[::2],n[1::2] = n[:len(n[::2])][::-1],n[len(n[::2]):][::-1]
