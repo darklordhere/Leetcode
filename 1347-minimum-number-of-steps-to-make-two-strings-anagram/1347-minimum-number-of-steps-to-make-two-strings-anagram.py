332d13b00b4e39a75ece61803dbeb8afca99cdd1
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-01-13 21:32:58 +0530

    Time: 89 ms (98.90%), Space: 18.1 MB (5.24%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,7 +1,14 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        a,b = Counter(s),Counter(t) ; c = 0
-        for i in b:
-            if i not in a: c += b[i]
-            elif a[i]<b[i]: c += b[i]-a[i] 
-        return c
+        # sol1 - mine
+        # a,b = Counter(s),Counter(t) ; c = 0
+        # for i in b:
+        #     if i not in a: c += b[i]
+        #     elif a[i]<b[i]: c += b[i]-a[i] 
+        # return c
+        
+        # sol2
+        # return (Counter(s)-Counter(t)).total()
+        
+        # sol3
+        return sum((Counter(s)-Counter(t)).values())
