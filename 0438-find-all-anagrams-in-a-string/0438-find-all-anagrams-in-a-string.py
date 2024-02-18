4da6afe5271d40eb7998c996b750d31f7e153062
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-02-18 21:14:02 +0530

    Time: 8434 ms (5.01%), Space: 18 MB (59.17%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -5,19 +5,9 @@ class Solution:
         pc = Counter(p)
         
         while j < len(s):
-            x = s[i:j+1]
-            
-            if len(x) < len(p):
-                j += 1
-            elif len(x) == len(p):
-                if Counter(x) == pc:
-                    l.append(i)
-                i,j=i+1,j+1
-                
-#             x = s[i:j+len(p)]
-            
-#             if Counter(x) == pc:
-#                 l.append(i)
-#             i,j=i+1,j+1
+            x = s[i:j+len(p)]     
+            if Counter(x)==pc:
+                l.append(i)
+            i,j=i+1,j+1
         return l
     
