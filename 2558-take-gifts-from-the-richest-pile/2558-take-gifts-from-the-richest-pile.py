c87ec69a99dcdd2b7dd82adcf58a5676bb4cfca3
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-12-12 21:43:27 +0530

    Time: 2 ms (91.28%), Space: 17.4 MB (20.60%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,7 +1,20 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        # while k != 0:
+        #     gifts = sorted(gifts)[::-1]
+        #     gifts[0] = int(math.sqrt(gifts[0]))
+        #     k -= 1
+        # return sum(gifts)
+        
+        
+        
+        
+        l = [-i for i in gifts]
+        heapify(l)
+    
         while k != 0:
-            gifts = sorted(gifts)[::-1]
-            gifts[0] = int(math.sqrt(gifts[0]))
+            sqr = int(math.sqrt(-heappop(l)))
+            heappush(l,-sqr)
             k -= 1
-        return sum(gifts)
+        return -sum(l)
+      
