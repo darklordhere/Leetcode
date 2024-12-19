2c911199302eae51360881088e5bcf295b3c52cf
SouravSingh49 <89247293+darklordhere@users.noreply.github.com>
2024-12-19 22:50:59 +0530

    Time: 0 ms (100.00%), Space: 17.6 MB (14.45%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.py
@@ -1,11 +1,8 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
 
-        
-        
         stack = []
         for i in arr:
-            print(stack)
             largest = i
             while stack and i < stack[-1]:
                 largest = max(largest,stack.pop())
